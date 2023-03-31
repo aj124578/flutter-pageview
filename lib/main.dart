2d51ab1d2c007ea1ffd34df96a59e3acb6132789
ìaj124578 <aj124578@naver.com>
2023-03-31 12:04:54 +0900

    page view

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: buildPageView(),
+    );
+  }
+
+  PageView buildPageView() {
+    return PageView.builder(
+      itemCount: 5,
+      itemBuilder: (context, index) {
+      return Image.network("https://picsum.photos/id/${index+1}/200/300", fit: BoxFit.cover,);
+    },);
+  }
+}
+
+
+
